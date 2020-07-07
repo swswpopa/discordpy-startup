0d6876f0c68663ba84a39c4bf6e0f101ea0e8385
swswpopa <59680766+swswpopa@users.noreply.github.com>
2020-07-08 05:43:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,10 +8,16 @@ TOKEN = os.environ['DISCORD_BOT_TOKEN']
 CHANNEL_ID = 730136347477540908 #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
-@client.event
-async def on_ready():
-    while True:
-        if time.strftime('%H:%M:%S',time.localtime())=='05:00:00':
-            channel = client.get_channel('チャンネルID')
-            await client.send_message(channel, '日付が変わりました')
+# 60秒に一回ループ
+@tasks.loop(seconds=60)
+async def loop():
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now == '05:00':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('日付が変わりました')  
+
+#ループ処理実行
+loop.start()
+# Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
