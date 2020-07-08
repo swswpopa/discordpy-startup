03272c5f39028fa4b8af794074e69d3372a43bb0
swswpopa <59680766+swswpopa@users.noreply.github.com>
2020-07-08 11:33:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,35 +1,21 @@
-#coding:UTF-8
-import discord
+from discord.ext import commands
 import os
-import time
-from discord.ext import tasks
-from datetime import datetime 
+import traceback
 
-TOKEN = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID = 730136347477540908 #チャンネルID
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event
-async def on_ready():
-    print('ready')
 
-async def on_message(message):
-	if message.author.bot: #自身や他Botの発言に反応しないようにする。
-		return
-	if "ありがと" in message.content:
-		await message.channel.send("どういたしまして！")
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
-# 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
-    # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
-	if now == '05:00':
-        channel = client.get_channel(CHANNEL_ID)
-        	await channel.send('日付が変わりました')  
 
-#ループ処理実行
-loop.start()
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+@bot.command()
+async def dbd(ctx):
+    await ctx.send('https://store.steampowered.com/app/381210/Dead_by_Daylight/')
+
+
+bot.run(token)
