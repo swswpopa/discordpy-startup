6b5c1c0d4025f08c8b73f452e3422d8f743f2e45
swswpopa <59680766+swswpopa@users.noreply.github.com>
2020-07-10 16:13:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,6 @@ import traceback
 from discord.ext import tasks
 from datetime import datetime, timedelta, timezone
 
-client = discord.Client()
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 CHANNEL_ID = 730136347477540908 #チャンネルID
