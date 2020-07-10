0cb0edff67cd136188136691dc23c794d1de8e41
swswpopa <59680766+swswpopa@users.noreply.github.com>
2020-07-10 16:50:43 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,12 +8,17 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 CHANNEL_ID = 730136347477540908 #チャンネルID
 
-
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)    
+    
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
     # /単語　で受け答え
 @bot.command()
