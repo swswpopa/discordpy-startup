2532acb3721fb58392194d21793d1d9281262875
swswpopa <59680766+swswpopa@users.noreply.github.com>
2020-07-27 14:06:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -91,7 +91,7 @@ async def loop():
     now = datetime.now().strftime('%H:%M')
     if now == '20:00':
         channel = bot.get_channel(CHANNEL_ID)
-        await channel.send('日付が変わりました！タスクキルしたらリアクションを付けましょうね')  
+        await channel.send('日付が変わりました！タスクキルしたらスタンプを付けましょうね')  
         await tmp_msg.add_reaction(emoji=":crab:")
           
 #ループ処理実行
