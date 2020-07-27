4f751fd7d52e92952c13c964e4487fe50a4b4aed
swswpopa <59680766+swswpopa@users.noreply.github.com>
2020-07-27 15:32:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -83,6 +83,7 @@ async def kaya(ctx):
 async def nyaru(ctx):
     await ctx.send('https://cdn.discordapp.com/attachments/620957812247363594/731098724406919179/image0.gif')  
     
-
+#ループ処理実行
+loop.start()
 
 bot.run(token)
