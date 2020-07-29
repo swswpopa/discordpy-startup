bee12276c1c68b35f9cba0d0ef0f21d99ba736ec
swswpopa <59680766+swswpopa@users.noreply.github.com>
2020-07-30 07:53:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -232,7 +232,7 @@ async def hiyori(ctx):
      
 @bot.command()
 async def list(ctx):
-    await ctx.send('プリコネキャラスキルモーション一覧 \n クリス：kurisu \n マコト：makoto \n 水マコト：mizumakoto \n ムイミ：muimi \n タマキ：tamaki \n オノン：onon \n 水菜：mizuna \n ネネカ：neneka \n プリペコ：puripeko \n 水キャル：mizukyaru \n プリコロ：purikoro \n 水サレン：mizusaren \n ニュッコロ：nyukkoro \n ハロキョ：harokyo \n ハロミソ：haromiso \n ハロミミ：haromimi \n バズル：bazuru \n クリチカ：kuritika \n ユニ：yuni \n レイ：rei \n スズナ：suzuna \n アン：an\n ミサト：misato \n 水マホ：mizumaho')
+    await ctx.send('```プリコネキャラスキルモーション一覧 \n クリス：kurisu \n マコト：makoto \n 水マコト：mizumakoto \n ムイミ：muimi \n タマキ：tamaki \n オノン：onon \n 水菜：mizuna \n ネネカ：neneka \n プリペコ：puripeko \n 水キャル：mizukyaru \n プリコロ：purikoro \n 水サレン：mizusaren \n ニュッコロ：nyukkoro \n ハロキョ：harokyo \n ハロミソ：haromiso \n ハロミミ：haromimi \n バズル：bazuru \n クリチカ：kuritika \n ユニ：yuni \n レイ：rei \n スズナ：suzuna \n アン：an\n ミサト：misato \n 水マホ：mizumaho \n ヒヨリ：hiyori```')
     await ctx.message.delete()   
      
 @tasks.loop(seconds=60)
