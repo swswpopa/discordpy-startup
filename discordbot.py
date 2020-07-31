9303c342517d34c704c0d975064d6159118dc1b2
swswpopa <59680766+swswpopa@users.noreply.github.com>
2020-07-31 11:47:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,7 +6,7 @@ import traceback
 from datetime import datetime
 from discord.ext import tasks
 
-bot = commands.Bot(command_prefix='/', help_command=None)
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
      
