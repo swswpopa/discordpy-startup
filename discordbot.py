ef8131c72d425a045ccda1c487d98fdd1b1e63d6
PoppyMania <52383716+PoppyMania@users.noreply.github.com>
2020-09-02 18:30:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -371,6 +371,13 @@ class BestUrl(commands.Cog, name = 'みんなのおすすめ'):
         gacha = ['10','20','30','40','50','60','70','80','90','100','110','120','130','140','150','160','170','180','190','200','210','220','230','240','250','260','270','280','290','300']
         await ctx.send(ctx.message.author.name + 'は' + random.choice(gacha) + '連で出ます' )
         await ctx.message.delete()
+        
+    @commands.command()
+    async def gachapopimasa(self,ctx):
+        """ガチャシミュレータ"""
+        
+        await ctx.send(ctx.message.author.name + 'は単発で出ます' )
+        await ctx.message.delete()
   
 
         
