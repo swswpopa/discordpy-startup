7885e7a728a82f6a7d0d0e776862cf079e30c258
swswpopa <59680766+swswpopa@users.noreply.github.com>
2020-10-23 10:58:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,28 +23,6 @@ class SkillMotionGIF(commands.Cog, name = 'スキルモーション'):
         super().__init__()
         self.bot = bot
         
-    @bot.event  
-    async def on_raw_reaction_add(payload):  
-        channel = client.get_channel(payload.channel_id)
-        if channel.id == ID_readme:
-            #ロールの付与
-            if str(payload.emoji) == '<:61ok:728923368870510605>':
-                guild = client.get_guild(payload.guild_id)  
-                member = guild.get_member(payload.user_id)  
-                role = guild.get_role(ID_clanmember)
-                if not member.bot:            
-                    await member.add_roles(role)  
-#リアクションを外すとロールも外れる                
-    @bot.event  
-    async def on_raw_reaction_remove(payload):  
-        channel = client.get_channel(payload.channel_id)
-        if channel.id == ID_readme:
-            if str(payload.emoji) == '<:61ok:728923368870510605>':
-                guild = client.get_guild(payload.guild_id)  
-                member = guild.get_member(payload.user_id)  
-                role = guild.get_role(ID_clanmember)
-                if not member.bot:            
-                    await member.remove_roles(role)        
 
     @commands.command()
     async def kurisu(self,ctx):
