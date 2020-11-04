b6bc9ee8e7efdbd58f08863d5457800a6d414cc0
swswpopa <59680766+swswpopa@users.noreply.github.com>
2020-11-04 20:13:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -483,15 +483,15 @@ bot.run(token)
 
 
 client = discord.Client()
-    @client.event  
-    async def on_raw_reaction_add(payload):  
-        channel = client.get_channel(payload.channel_id)
-        if channel.id == ID_readme:
-            #ロールの付与
-            if str(payload.emoji) == '<:61ok:728923368870510605>':
-                guild = client.get_guild(payload.guild_id)  
-                member = guild.get_member(payload.user_id)  
-                role = guild.get_role(ID_clanmember)
-                if not member.bot:            
-                    await member.add_roles(role)     
+@client.event  
+async def on_raw_reaction_add(payload):  
+    channel = client.get_channel(payload.channel_id)
+    if channel.id == ID_readme:
+#ロールの付与
+        if str(payload.emoji) == '<:61ok:728923368870510605>':
+            guild = client.get_guild(payload.guild_id)  
+            member = guild.get_member(payload.user_id)  
+            role = guild.get_role(ID_clanmember)
+            if not member.bot:            
+                await member.add_roles(role)     
 client.run(token)  
